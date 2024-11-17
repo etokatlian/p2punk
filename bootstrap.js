59bda5896a7fc6f38bbebce85e32b722a73742c9
Michael Miller-Hairston <michael.millerhairston@gmail.com>
2024-11-17 10:35:36 -0700

    Add deploy workflow

--- bootstrap.js
+++ bootstrap.js
@@ -1,11 +1,12 @@
-import { P2PServer } from "./server/wss.js";
-import { PeerRepository } from "./server/peers/peerRepository.js";
-import { MessageService } from "./server/messages/messageService.js";
+import { P2PServer } from './server/wss.js';
+import { PeerRepository } from './server/peers/peerRepository.js';
+import { MessageService } from './server/messages/messageService.js';
 
 export const createWss = (port) => {
   const peerRepository = new PeerRepository();
   const messageService = new MessageService();
 
+  console.log(`Server running on ${port}...`);
   return new P2PServer(port, peerRepository, messageService).initialize();
 };
 
